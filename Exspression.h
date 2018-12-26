a505f94e8a701f76ef8fe9a348cef3ba261416c4
fishmanyael <39657103+fishmanyael@users.noreply.github.com>
2018-12-26 13:53:43 +0200

    Add files via upload

---
+++ Exspression.h
@@ -0,0 +1,18 @@
+//
+// Created by yael on 25/12/2018.
+//
+
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Expression {
+public:
+    virtual double calculate() = 0;
+};
+
+#endif
